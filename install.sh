17a31158bf6893a4b76bb6e533fa997e8cc20d71
nikola.reljin <nikola.reljin@gmail.com>
2018-08-23 22:14:10 -0400

    alternative script run

--- install.sh
+++ install.sh
@@ -1,34 +1,45 @@
 #!/bin/bash
 
 # update
-sudo apt-get update
+sudo apt-get update -y
+
+# Uninstall old versions
+sudo apt-get remove docker docker-engine docker.io
 
 # prerequisites
 sudo apt-get install apt-transport-https \
                        ca-certificates \
-                       software-properties-common
+                       software-properties-common -y
 
-# add GPG key
-curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
-# verify key
-apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+curl -fsSL https://get.docker.com -o get-docker.sh
+chmod +x get-docker.sh
+sudo sh get-docker.sh
 
-# install latest Docker raspbian repo
-sudo add-apt-repository \
-       "deb https://apt.dockerproject.org/repo/ \
-       raspbian-$(lsb_release -cs) \
-       main"
+# @TODO: enable if having issues with the script
+## add GPG key
+#curl -fsSL https://yum.dockerproject.org/gpg | sudo apt-key add -
+## verify key
+#apt-key fingerprint 58118E89F3A912897C070ADBF76221572C52609D
+#
+## install latest Docker raspbian repo
+#sudo add-apt-repository \
+#       "deb https://apt.dockerproject.org/repo/ \
+#       raspbian-$(lsb_release -cs) \
+#       main"
+#
+## alternative:
+## sudo vim /etc/apt/sources.list
+## add this:
+## https://apt.dockerproject.org/repo/ raspbian-jessie main
+#
+## update
+#sudo apt-get update -y
+#
+## install Docker
+#sudo apt-get -y install docker-engine -y
 
-# alternative:
-# sudo vim /etc/apt/sources.list
-# add this:
-# https://apt.dockerproject.org/repo/ raspbian-jessie main
-
-# update
-sudo apt-get update
-
-# install Docker
-sudo apt-get -y install docker-engine
+sudo apt-get update -y
+sudo apt-get install docker-compose -y
 
 # test
 docker run hello-world
